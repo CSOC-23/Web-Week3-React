7d30de4fd906429b5cbede1fef35824cd1a214a1
Sanskruti Kolgane <96910867+Sansgithub22@users.noreply.github.com>
2023-07-11 10:15:09 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,29 +4,32 @@ import { useEffect, useState } from "react";
 import axios from "../utils/axios";
 import { useAuth } from "../context/auth";
 
-export default function Home() {
-	const { token } = useAuth();
-	const [tasks, setTasks] = useState([]);
-
-	function getTasks() {
-		/***
-		 * @todo Fetch the tasks created by the user.
-		 * @todo Set the tasks state and display them in the using TodoListItem component
-		 * The user token can be accessed from the context using useAuth() from /context/auth.js
-		 */
+export default function Home() {const { token } = useAuth();const [tasks, setTasks] = useState([]);useEffect(() => {getTasks();}, []);
+	async function getTasks() {
+		try {
+			console.log("called")
+			let resp = await axios.get("todo/", {headers: {Authorization: `Token ${token}`,},});
+			setTasks(resp.data);
+		} catch (err) {return [];}
 	}
-
+	function removeHandler(task)
+	{
+		return () => {
+			console.log("removing ",task);setTasks((tasks) => tasks.filter((k) => k != task));console.log(tasks.filter((k) => k != task));};
+	}
+	
 	return (
-		<div>
-			<center>
-				<AddTask />
-				<ul className="flex-col mt-9 max-w-sm mb-3 ">
-					<span className="inline-block bg-blue-600 py-1 mb-2 px-9 text-sm text-white font-bold rounded-full ">
+		<div><center><AddTask token={token} add={getTasks}/> <ul className="flex-col mt-9 max-w-sm mb-3 ">
+					<span className="inline-block bg-yellow-600 py-1 mb-2 px-9 text-sm text-white font-bold rounded-full ">
 						Available Tasks
 					</span>
-					<TodoListItem />
+					{tasks.map((task, i) => {
+						return <TodoListItem task={task} key={i} onRemove={getTasks} token={token}/>;
+					})}
 				</ul>
 			</center>
 		</div>
 	);
 }
+
+
