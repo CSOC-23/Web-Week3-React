2adf18f7d22db1a0614f891fc5fc4ed4a5226e1e
Sanskruti Kolgane <96910867+Sansgithub22@users.noreply.github.com>
2023-07-11 09:28:19 +0530

    commit

--- components/AddTask.js
+++ components/AddTask.js
@@ -1,25 +1,25 @@
-export default function AddTask() {
-	const addTask = () => {
-		/**
-		 * @todo Complete this function.
-		 * @todo 1. Send the request to add the task to the backend server.
-		 * @todo 2. Add the task in the dom.
-		 */
+import axios from "../utils/axios";
+import { useState } from "react";
+export default function AddTask({ add, token }) {
+	const addTask = async () => {
+	try {let id = await axios.post(
+		"todo/create/",{title,},
+				{headers: {Authorization: `Token ${token}`,"Content-Type": "application/json",},
+				}
+			);
+setTitle("");add();
+		} catch (err) {console.log(err);}
 	};
-
+	const [title, setTitle] = useState("");
 	return (
-		<div className="flex items-center max-w-sm mt-24">
+		<div className= "flex items-center max-w-sm mt-24">
 			<input
 				type="text"
 				className="todo-add-task-input px-4 py-2 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm border border-blueGray-300 outline-none focus:outline-none focus:ring w-full"
 				placeholder="Enter Task"
+				onChange={(e) => {setTitle(e.target.value);}}
+				value={title}
 			/>
-			<button
-				type="button"
-				className="todo-add-task bg-transparent hover:bg-green-500 text-green-700 text-sm hover:text-white px-3 py-2 border border-green-500 hover:border-transparent rounded"
-				onClick={addTask}>
-				Add Task
-			</button>
-		</div>
+			<button type="button" className="todo-add-task bg-transparent hover:bg-yellow-500 text-green-700 text-sm hover:text-white px-3 py-2 border border-green-500 hover:border-transparent rounded" onClick={addTask}>Add Task</button></div>
 	);
 }
